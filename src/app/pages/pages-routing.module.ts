70b6ad5627184477f00fef0e593275bc7b6e38e1
ahadsenja <hamdanilm96@gmail.com>
2022-04-21 10:55:05 +0800

    chore: update pages routing modules

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LandingComponent } from './landing/landing.component';
+import { ProjectComponent } from './project/project.component';
+import { SpeakerComponent } from './speaker/speaker.component';
+import { GalleryComponent } from './gallery/gallery.component';
 import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'project', component: ProjectComponent },
+  { path: 'speaker', component: SpeakerComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
